a6367b26231582c9c783190680b0c0b85ebc9d4f
Peculiar <onugbolup2019@gmail.com>
2022-08-12 07:56:45 +0100

    Add project files.

---
+++ INEC Assignment/INEC Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INEC_Assignment
+{
+    internal class INEC_Project
+    {
+        public void VoterReg(List<string> InecReg)
+        {
+            string output;
+            Console.WriteLine("Enter Voters Name");
+            string newVoterName=Console.ReadLine();
+            foreach (string name in InecReg)
+            {
+                if (name == newVoterName)
+                {
+                    Console.WriteLine("This name has been registered"); 
+
+                }
+            }
+
+        }
+    }
+}
---
+++ INEC Assignment/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace INEC_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// list of Arrays to loop through
+            List<string> InceReg = new List<string> {
+
+                "Peter Ojo",
+                "favour Ahmed",
+                "Musa Litten",
+                "Mmasi Faith",
+                "Kate Philip",
+                "favour Ahmed",
+                "Musa Litten",
+                "John peterson",
+                "felix kate",
+                "Godsent Isa",
+                "Peter Ojo",
+                "Mmasi Faith",
+                "Temi liun",
+                "Kemi Alex"
+            };
+            IDictionary<string, int> InceRegDic = new Dictionary<string, int>();
+            //use of hashSet to sort out the names and findout how times the a person name appear.
+            HashSet<string> InecRegUnique=InceReg.ToHashSet();
+            foreach (var name in InceReg)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\n\n\n");
+            foreach (var hashName in InecRegUnique)
+            {
+                int count = 0;
+                foreach(var name in InceReg)
+                {
+                    if (name == hashName)
+                    {
+                        count++; 
+                    }
+
+                }
+
+                InceRegDic.Add(hashName, count);
+            }
+
+            foreach (var kv in InceRegDic)
+            {
+                if (kv.Value == 1)
+
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"{kv.Key} registered {kv.Value}times");
+                }
+
+            }
+            
+        }
+    }
+}
+
+
